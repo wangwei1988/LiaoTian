80c35954b7bc3719dcb869bfcebd3b07af6a99a9
great_king <297295825@qq.com>
2016-12-04 21:53:32 +0800

    修改一些图片

--- 聊天界面/聊天界面/Message.m
+++ 聊天界面/聊天界面/Message.m
@@ -1,8 +1,8 @@
 //
-//  Message.m
-//  融云测试
+//  AppDelegate.h
+//  聊天界面
 //
-//  Created by  王伟 on 2016/12/3.
+//  Created by  王伟 on 2016/12/4.
 //  Copyright © 2016年  王伟. All rights reserved.
 //
 
